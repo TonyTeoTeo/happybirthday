d8774ee72980c5ff9397fd3ee62b48a1e9fac85c
unknown <you@example.com>
2023-11-30 19:25:32 +0700

    first commit

---
+++ song.js
@@ -0,0 +1,15 @@
+let songs = [
+    {
+        name: 'HAPPY BIRTHDAY',
+        path: 'music/happybirthday.mp3',
+        artist: 'Thúy Trinh 12/12/2023',
+        image: 'tt.jpg'
+    },
+    {
+        name: 'HAPPY BIRTHDAY',
+        path: 'music/happybirthday.mp3',
+        artist: 'Thúy Trinh',
+        image: 'tt.jpg'
+    }
+    
+];
